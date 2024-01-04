08f2949e31f0d70a09499c474a766bb81f5e9157
admin <admin@DESKTOP-COURELD>
2024-01-04 08:50:23 +0530

    initial commit

---
+++ operator/src/operator/conti.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class conti {
+	public static void main(String[] args) {
+
+		int age = 15;
+
+		if (age >= 18) {
+
+			System.out.println("Eligible to vote in india");
+
+		}
+
+		else {
+
+			System.out.println("Not eligible to vote in india");
+		}
+
+	}
+}
